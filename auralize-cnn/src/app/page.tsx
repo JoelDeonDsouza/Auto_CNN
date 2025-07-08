b0d8158c173dbdfb41eefa725c8afe80221cbdc6
joeldeondsouza <jdeondsouza@gmail.com>
2025-07-08 14:27:13 +0200

    feat(frontend): initialize Next.js project to enable visual rendering

---
+++ auralize-cnn/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function HomePage() {
+  return (
+    <main className="min-h-screen bg-sky-50 p-8">
+      <h1>Hello world</h1>
+    </main>
+  );
+}
